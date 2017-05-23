0feb222f9fa33c37a048c938810707532be1709e
kkhalifah <karen.khalifah@gmail.com>
2017-05-22 23:25:26 -0400

    first commit

---
+++ words.js
@@ -0,0 +1,5 @@
+var fiveSyllablesFirst = ["breaking with river", "behind the water", "throughout the water,", "Sweden stops weaving.", "Panda works quickly", "admiring my yard," , "hark the sweeping taught!", "Tiger grunts aptly", "balancing my face,", "summoning my fate,", "before the sugar,", "sweeping with river", "into the water"];
+
+var sevenSyllables = ["fingers, Emperor admired", "China pondered happily.", "You considered the elbows", "Panda admired finally.","sounds! sounds! sweeping breaking shoe.", "fie! hark! leaving basking dog.", "Panda hunts brightly", "Brazil measured my blossoms", "Panda approved my blossoms", "Japan smiling happily.", "bright princes rejoiced quickly", "You discovered the crayons", "You imagined the flowers"];
+
+var fiveSyllablesLast = ["While balancing the","beside the water", "Japan stops breaking.", "admiring my face" , "into my sugar", "sounds the blowing wind!", "drat the leaving played!" , "Paris falls strangely", "under the gravel" , "behind the water", "tired princes agreed" , "under the sugar", "feeling the river"]
